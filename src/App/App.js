5c9377e02c804e285352027f7535dfa51fde487a
JingeLi <jinge.li@thoughtworks.com>
2020-06-30 23:53:38 +0800

    add Router

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.scss';
-import Chat from '../Chat/Chat';
+import Welcome from "../Welcome/Welcome";
+import Chat from "../Chat/Chat";
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        <Chat />
+        <Router>
+          <Route path="/" component={Welcome}/>
+          <Route path="/chat" component={Chat}/>
+          <Route path="/activity" component={Welcome}/>
+          <Route path="/about" component={Welcome}/>
+        </Router>
       </div>
     );
   }
